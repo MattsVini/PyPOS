752bf9d5ddef69f7fc74f012d36192314006e09c
Mateus Vinicios Guimarães Rios de Assis <mateusvinicios214@gmail.com>
2026-04-01 11:11:05 -0300

    Try call Render

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import database
 import os
 from dotenv import load_dotenv
-
 app = FastAPI()
-URL_DATABASE = os.environ["URL_DATABASE"]
-print(URL_DATABASE)
+@asynccontextmanager
+async def lifespan(app):
+    URL_DATABASE = os.environ["URL_DATABASE"]
+    database.create_table(URL_DATABASE)
+    print("done DB!")
+    yield
 
+app = FastAPI(lifespan=lifespan)
 @app.get("/")
-def request_database(URL_DATABASE):
-    return database.create_table((URL_DATABASE))
-
-request_database(URL_DATABASE)
+def request_database():
+    return {"status: online"}
